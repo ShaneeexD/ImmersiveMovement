6b20e022b1525abb21767f5fe2679278ef0151e3
ShaneeexD <31047364+ShaneeexD@users.noreply.github.com>
2024-10-20 11:42:16 +0100

    added lean speed + camera jolt

--- ImmersiveMovement/PatchClass.cs
+++ ImmersiveMovement/PatchClass.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace ImmersiveMovement
 {
-    [BepInPlugin("ImmersiveMovement", "ImmersiveMovement", "1.0.1")]
+    [BepInPlugin("ImmersiveMovement", "ImmersiveMovement", "1.1.0")]
     public class ImmersiveMovement : BasePlugin
     {
         public static ManualLogSource Logger;
@@ -16,6 +16,9 @@ namespace ImmersiveMovement
 
         public static ConfigEntry<float> walkSpeedMultiplier;
         public static ConfigEntry<float> runSpeedMultiplier;
+        public static ConfigEntry<float> leanSpeedMultiplier;
+        public static ConfigEntry<bool> cameraJolt;
+
 
         public override void Load()
         {
@@ -25,9 +28,13 @@ namespace ImmersiveMovement
             walkSpeedMultiplier = Config.Bind("Player", "Walk Speed", 1.7f, "Player walking speed. (Game default 4)");
             runSpeedMultiplier = Config.Bind("Player", "Run Speed", 3.0f, "Player running speed. (Game default 8)");
 
+            leanSpeedMultiplier = Config.Bind("Player", "Lean Speed", 2.0f, "Player lean speed. (Game default 10)");
+            cameraJolt = Config.Bind("Player", "Camera Jolt", true, "Less camera jolt from falling.");
+
             Logger.LogInfo($"Walk speed multiplier: {walkSpeedMultiplier.Value}");
             Logger.LogInfo($"Run speed multiplier: {runSpeedMultiplier.Value}");
-
+            Logger.LogInfo($"Lean speed multiplier: {leanSpeedMultiplier.Value}");
+            Logger.LogInfo($"Override camera jolt: {cameraJolt.Value}");
             try
             {
                 harmony = new Harmony("ImmersiveMovement");
--- ImmersiveMovement/PlayerPatch.cs
+++ ImmersiveMovement/PlayerPatch.cs
@@ -1,13 +1,14 @@
 ﻿using HarmonyLib;
 using UnityEngine;
 using System.Reflection;
+using UnityStandardAssets.Characters.FirstPerson;
 
 namespace ImmersiveMovement
 {
     [HarmonyPatch(typeof(Player), "FixedUpdate")] 
     public class PlayerPatch
     {
-        private static bool logSent = false; 
+        private static bool logSent = false;
 
         [HarmonyPrefix]
         public static void Prefix(Player __instance)
@@ -36,6 +37,30 @@ namespace ImmersiveMovement
             else
             {
                 ImmersiveMovement.Logger.LogWarning("Player instance is null.");
+            }
+        }
+
+        [HarmonyPatch(typeof(FirstPersonController), "Update")]
+        public class LeanSpeedPatch
+        {
+            private static bool logSent = false;
+            static void Postfix(FirstPersonController __instance)
+            {
+                if (!logSent) 
+                {
+                    ImmersiveMovement.Logger.LogInfo("Player lean speed modified.");
+                    logSent = true;
+                }
+                __instance.leanSpeed = ImmersiveMovement.leanSpeedMultiplier.Value;               
+            }
+
+            [HarmonyPatch(typeof(FirstPersonController), "JoltCamera")]
+            static void Prefix(ref float amplitude, Vector3 direction, float speed)
+            {
+                if (ImmersiveMovement.cameraJolt.Value == true)
+                {
+                    amplitude = Mathf.Min(amplitude, 30f);
+                }
             }
         }
     }
